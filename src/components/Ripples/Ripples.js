d561f86acff0b026a8327b9c94d5daa5264a3497
66RING <1552087953@qq.com>
2020-08-11 22:38:10 +0800

    init

---
+++ src/components/Ripples/Ripples.js
@@ -0,0 +1,43 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./Ripples.css"
+
+
+
+
+export default class Ripples extends React.Component{
+
+  componentDidMount(){
+    const buttons = document.querySelectorAll('a')
+    buttons.forEach(btn => {
+      btn.addEventListener('click', function(e){
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement('span')
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+          ripples.remove()
+        }, 1000)
+      })
+    })
+  }
+
+
+
+  render(){
+    return (
+      <div class="ripples">
+        <a href="#">Button</a>
+        <a href="#">Button</a>
+      </div>
+    )
+  }
+
+}
+
+
+
---
+++ src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Ripples from "../components/Ripples/Ripples"
+
+
+export default class Main extends React.Component{
+render(){
+  return (
+    <div class="home">
+      <Ripples></Ripples>
+    </div>
+
+
+  )
+}
+}
+
