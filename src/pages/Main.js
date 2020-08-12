1b42b6a3607806defd4775042a76b9629b9d12af
66RING <1552087953@qq.com>
2020-08-12 17:32:44 +0800

    drop down menu

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Ripples from "../components/Ripples/Ripples"
+import DropDownMenu from "../components/DropDownMenu/DropDownMenu"
 
 
 export default class Main extends React.Component{
 render(){
   return (
     <div class="home">
-      <Ripples></Ripples>
+      <DropDownMenu></DropDownMenu>
     </div>
 
 
